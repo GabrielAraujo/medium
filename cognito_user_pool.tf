e0f581fff67f7c5cafffd86f73b398e8e7f39be1
Gabriel Araujo <gabrielaraujo@MacBook-Pro-de-Gabriel.local>
2020-06-14 14:17:19 -0300

    small adjustments to email message

--- cognito_user_pool.tf
+++ cognito_user_pool.tf
@@ -35,6 +35,8 @@ resource "aws_cognito_user_pool" "user_pool" {
 
   verification_message_template {
     default_email_option = "CONFIRM_WITH_CODE"
+    email_subject = "Account Confirmation"
+    email_message = "Your confirmation code is {####}"
   }
 
   schema {
